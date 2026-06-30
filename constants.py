f57ebf5916b74c4afc9a9302afe6651ac736c9fb
swordgeo <geo.purdue@gmail.com>
2026-06-30 08:19:47 -0400

    CH3: L2: Draw Player

--- constants.py
+++ constants.py
@@ -1,2 +1,6 @@
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+# 3.2
+PLAYER_RADIUS = 20
+LINE_WIDTH = 2 # width of the line that draw the player ship
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     # print("Hello from geoboot-asteroid!")
@@ -9,6 +10,10 @@ def main():
     # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -16,7 +21,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        # print(dt)
 
 
 if __name__ == "__main__":

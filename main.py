6e6ffbfa51307d687918cef5080a09f9092e4cd8
swordgeo <geo.purdue@gmail.com>
2026-07-06 08:44:20 -0400

    CH4: L1: Asteroid and AsteroidField Class

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     # print("Hello from geoboot-asteroid!")
@@ -18,8 +20,14 @@ def main():
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
 
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # 4.1
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
 
 
     while True:
